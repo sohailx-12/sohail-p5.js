fa123d27cea9379cce581095ca76ffde40aa995f
sohailx-12 <159238487+sohailx-12@users.noreply.github.com>
2024-06-10 13:01:34 +0400

    Create Car.js

---
+++ Car.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //background color
+  background(14,144,101);
+  rect(150,150,100,50);  
+  //left tire
+  ellipse(160,200, 35, 35);  
+  //right tire
+  ellipse(250,200, 35, 35);  
+  //upper tire
+  ellipse(200,150, 35, 35);
+}
